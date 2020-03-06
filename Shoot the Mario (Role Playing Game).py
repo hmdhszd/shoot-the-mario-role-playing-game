9f83e582cb99e858be40aea68dc35ff9f5bd2aca
Hamid Hosseinzadeh <hamidhszd@gmail.com>
2020-03-06 13:17:34 +0330

    added first page for selecting bullet

--- Shoot the Mario (Role Playing Game).py
+++ Shoot the Mario (Role Playing Game).py
@@ -7,8 +7,6 @@ from PIL import Image, ImageTk
 
 
 
-
-
 class Bullet: 
     def levelup(self):
         print(self.levelup_degree)
@@ -28,26 +26,18 @@ class Turtle(Bullet):
         self.levelup_degree = 20
 
 
-player = Turtle()
-player.levelup()
-
-player = Mushroom()
-player.levelup()
 
 
-root = Tk()
+
+
+x = 1
 
 
 mushroomx= 1310
 mushroomy= 650
 
 
-def setBG():
-    load = Image.open("bg2.png")
-    render = ImageTk.PhotoImage(load)
-    img = Label(root, image=render)
-    img.image = render
-    img.place(x=0, y=0)
+
 
 
 def placemushroom(x,y):
@@ -66,6 +56,14 @@ def placemario(x,y):
     img.place(x=x, y=y)
 
 
+def PlaceImage(x,y,img,window):
+    load = Image.open(img)
+    render = ImageTk.PhotoImage(load)
+    img = Label(window, image=render)
+    img.image = render
+    img.place(x=x, y=y)
+
+
 
 def update(): 
     global mushroomx
@@ -73,10 +71,10 @@ def update():
     lbl.config(text = "Fired!")
     #lbl.config(text = strftime('%H:%M:%S %p'))
     lbl.after(300, update)
-    setBG()
+    PlaceImage(0,0,"bg.png",root)
     if (mushroomx <= 0) or (mushroomy <= 0):
         print("outttt")
-        pass
+        exit()
     else:
         mushroomx -= int(speedcombo.get())
         mushroomy -= int(Powercombo.get())
@@ -86,10 +84,85 @@ def update():
 
 
 
-while True:
+def SelectMushroom():
+    global player
+    player = Mushroom()
+    selectplayer.destroy()
 
-    setBG()
-    
+def SelectTurtle():
+    global player
+    player = Turtle()
+    selectplayer.destroy()
+
+
+
+
+selectplayer = Tk()
+PlaceImage(-600,-500,"bg.png",selectplayer)
+
+titre = Label(selectplayer,text= "Select Your Bullet" , font = ('arial', 20, 'bold'), foreground = 'red')
+titre.pack(anchor = 'center') 
+
+
+
+PlaceImage(80,180,"mushroom.jpg",selectplayer)
+lbl1 = Label(selectplayer,text= "Mushroom", font = ('arial', 20, 'bold'), foreground = 'red')
+lbl1.place(x=50,y=120)
+
+lbl3 = Label(selectplayer,text= "Successful Shot:", font = ('arial', 15, 'bold'), foreground = 'red')
+lbl3.place(x=35,y=270)
+
+
+lbl5 = Label(selectplayer,text= "+10 Points", font = ('arial', 20, 'bold'), foreground = 'red')
+lbl5.place(x=50,y=300)
+
+btn = Button(selectplayer, text="Select Mushroom", command=SelectMushroom)
+btn.place(x=55,y=340)
+
+
+
+
+
+
+
+
+PlaceImage(370,195,"turtle.png",selectplayer)
+lbl2 = Label(selectplayer,text= "Turtle", font = ('arial', 20, 'bold'), foreground = 'red')
+lbl2.place(x=353,y=120)
+
+lbl4 = Label(selectplayer,text= "Successful Shot:", font = ('arial', 15, 'bold'), foreground = 'red')
+lbl4.place(x=310,y=270)
+
+lbl6 = Label(selectplayer,text= "+20 Points", font = ('arial', 20, 'bold'), foreground = 'red')
+lbl6.place(x=325,y=300)
+
+btn1 = Button(selectplayer, text="Select Turtle", command=SelectTurtle)
+btn1.place(x=348,y=340)
+
+
+
+
+
+
+
+
+selectplayer.geometry("529x460")
+selectplayer.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+for i in range(0,x):
+    root = Tk()
+    PlaceImage(0,0,"bg.png",root)
+    print(player.name)
     mariox = random.randrange(50, 850)
     marioy = random.randrange(50, 650)
     placemario(mariox,marioy)
